17b86e5c0b707a9df66ebabcd7002a868bf388bb
Gilmar Sanchez <gsanchez@tsgroup.com.co>
2022-03-21 15:42:50 -0500

    Formas de pasar los datos del controller al view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,14 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = ['Gilmar', 'Juan', 'Maria', 'Natalia', '<script>alert("hola")</script>'];
-        return view('users', ['users' => $users, 'title' => 'Listado de Usuarios']);
+        $users = ['Gilmar', 'Juan', 'Maria', 'Natalia', '<script>alert("hola")</script>','Evelyn'];
+        $title = 'Listado de Usuarios';
+        // return view('users')
+        //         ->with('users', $users)
+        //         ->with('title', $title);
+        
+        //Otra forma de pasar los datos a  la vista
+        return view('users', compact('title', 'users'));
     }
 
     public function show($id)
